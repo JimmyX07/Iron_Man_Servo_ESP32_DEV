6f2e95419163dbde75f6ac2f7ff4296e01313062
Leonardo Congiu <leonardocongiu@iMac.station>
2025-05-05 12:21:36 +0200

    random

--- src/configuration.h
+++ src/configuration.h
@@ -12,11 +12,11 @@ If you're using a J.I.M. board that I gave you you don't need to modify the "PIN
 #define SERVO2_CLOSE_POS 160
 
 // PINS
-#define BUTTON_PIN 7
-#define RIGHT_EYE 8
+#define BUTTON_PIN 4
+#define RIGHT_EYE 2
 #define LEFT_EYE 15
 #define SERVO1_PIN 21
-#define SERVO2_PIN 20
+#define SERVO2_PIN 19
 
 // SERVO PWM VALUES, modify them according to the specs of the servos you are using
 #define PWM_LOW 500
